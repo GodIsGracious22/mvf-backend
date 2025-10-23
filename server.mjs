5e6ff45500b963040def2d65841b6323387adeda
Austin Gentry <austingentry@Austins-Mini.lan>
2025-10-22 22:31:42 -0400

    Add Plaid categorized transactions route for Prime users

--- server.mjs
+++ server.mjs
@@ -107,5 +107,5 @@ app.post("/login", (req, res) => {
 });
 
 // ✅ Start Server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
